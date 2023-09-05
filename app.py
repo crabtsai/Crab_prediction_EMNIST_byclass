596b5aa9868973ab0b1e7af1d94e075684d2055e
crabtsai <87925842+crabtsai@users.noreply.github.com>
2023-09-05 10:30:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from tensorflow.keras.models import model_from_json
 model = tf.keras.models.load_model('model.h5')
 st.title('Crab_英文字母大小寫及數字辨識')
 col1, col2 = st.columns(2)
+
 with col1:
     # Create a canvas component
     canvas_result = st_canvas(
